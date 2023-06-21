9df97c573f7cc64468ee4ceedee91de7655f1e65
hyunsu00 <hyun955807@naver.com>
2023-06-21 18:49:13 +0900

    minizipTest vcpkg빌드 추가

--- README.md
+++ README.md
@@ -9,3 +9,11 @@ $ git submodule add https://github.com/emscripten-core/emsdk sdks/emsdk
 # 서브모듈 업데이트
 $ git submodule update --init --recursive
 ```
+
+## vcpkg 
+```bash
+# 처음 git clone 시작시 실행
+$ cd sdks/    
+$ ./vcpkg/bootstrap-vcpkg.sh
+$ ./vcpkg/vcpkg install minizip >> ./../logs/minizip.txt
+```
---
+++ minizipTester/minizipTester.c
@@ -0,0 +1,29 @@
+#include <stdio.h> // printf
+#include "miniunz.h"
+#include <sys/stat.h> // mkdir
+
+/*
+S_IRUSR : (00400) - owner에 대한 읽기 권한
+S_IWUSR : (00200) - owner에 대한 쓰기 권한
+S_IXUSR : (00100) - owner에 대한 search 권한
+S_IRGRP : (00040) - Group에 대한 읽기 권한
+S_IWGRP : (00020) - Group에 대한 쓰기 권한
+S_IXGRP : (00010) - Group에 대한 search 권한
+S_IROTH : (00004) - Other에 대한 읽기 권한
+S_IWOTH : (00002) - Other에 대한 쓰기 권한
+S_IXOTH : (00001) - Other에 대한 search 권한
+== 0777
+*/
+#define	S_FULL	(S_IRWXU|S_IRWXG|S_IRWXO)
+#define DR_NAME "result"
+
+int main(int argc, char *argv[])
+{
+    printf("minizipTester\n");
+
+    mkdir(DR_NAME, S_FULL);
+    int ret = unzip("sample.hwpx", DR_NAME);
+
+    printf("unz() ret = %d\n", ret);
+    return 0;
+}
